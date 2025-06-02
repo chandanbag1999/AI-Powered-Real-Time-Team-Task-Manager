7fa4407a267b8cb3a273afa83e3e03254c6d5a7b
chandan bag <cbag67612@gmail.com>
2025-06-02 23:43:53 +0530

    Add authentication middleware and secure routes
    - Implemented `protect` middleware to verify JWT tokens and attach user information to requests.
    - Added `adminOnly` middleware to restrict access to admin users.
    - Updated authentication routes to include a protected route for user dashboard access.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const AuthController = require("../controllers/AuthController");
+const {protect, adminOnly} = require("../middlewares/authMiddleware");
+
 
 
 
@@ -8,5 +10,12 @@ const router = express.Router();
 
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
+
+router.get('/me', protect, adminOnly, (req, res) => {
+  res.json({
+    message: 'Welcome to your dashboard',
+    user: req.user
+  });
+});
 
 module.exports = router;
